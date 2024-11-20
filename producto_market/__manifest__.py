780cd78aef0c41e8ee8c22dda08ad4009219606d
ferguez1989 <f.rodriguez@beeren.com.mx>
2024-11-20 12:08:03 -0600

    C

--- producto_market/__manifest__.py
+++ producto_market/__manifest__.py
@@ -1,7 +1,7 @@
 {
   "version": "17.0.1.0",
   "license": "LGPL-3",
-  "author": "Fernando Rodriguez C",
+  "author": "Fernando Rodriguez Co",
   "name": "Producto Market",
   "depends": [
     "product",
