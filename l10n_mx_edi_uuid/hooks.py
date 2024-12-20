800e91983b6b201400a2bfb3bd4481bd0baeb8c5
ferguez1989 <f.rodriguez@beeren.com.mx>
2024-12-20 12:48:38 -0600

    Sin Plugins
    Eliminamos los extras

--- l10n_mx_edi_uuid/hooks.py
+++
@@ -1,8 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import SUPERUSER_ID, api
-
-
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['ir.attachment'].update_all_uuids()
